f441a930d8165dc914a54adef9fe943443592001
zapalm <zapalm@ya.ru>
2015-02-09 18:14:27 +1000

    // first release

---
+++ ru.php
@@ -0,0 +1,19 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_30bf4cb78ade00b21717fcd168a3f9be'] = 'Заказ при наличии';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_deb04f239dd2945e95e92dd44c9bae62'] = 'Позволяет оставить заявку на товар, которого пока нет в наличии';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_bec715e18172248f95f9ef436944042e'] = 'Под заказ';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_35df4f94e18fe021af7dfc5c5315b9aa'] = 'Все поля обязательные.';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_9d4466fabb9ab94f2b4f9ab17d78c959'] = 'Заказ не отправлен. Попробуйте еще раз.';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_b22fa58b744a6e686c08e7cb2388d7c1'] = 'Отправлен.';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_c2e924fa4ebac82470869dd8bada7600'] = 'Форма заказа';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_7e12dcf54cf118e0a4a8e29ff117cfc0'] = 'Заполните форму заказа товара';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_49ee3087348e8d44e1feda1917443987'] = 'Имя';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_8e55577a8ed3b20bff65d23c000280d4'] = 'Фамилия';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_bcc254b55c4a1babdf1dcb82c207506b'] = 'Телефон';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_1e884e3078d9978e216a027ecd57fb34'] = 'E-mail';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_57d056ed0984166336b7879c2af3657f'] = 'Город';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_0be8406951cdfda82f00f79328cf4efc'] = 'Комментарий';
+$_MODULE['<{blockbackorder}prestashop>blockbackorder_a4d3b161ce1309df1c4e25df28694b7b'] = 'Отправить';
