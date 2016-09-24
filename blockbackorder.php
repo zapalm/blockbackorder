25b3333f2b6c528b41916f2e026c1152dfe028bb
zapalm <zapalm@ya.ru>
2016-09-25 09:52:17 +1000

    // code format

--- blockbackorder.php
+++ blockbackorder.php
@@ -2,95 +2,91 @@
 /**
  * Block Back Order: module for PrestaShop 1.2-1.6
  *
- * @author zapalm <zapalm@ya.ru>
- * @copyright (c) 2011-2015, zapalm
- * @link http://prestashop.modulez.ru/en/ Homepage
- * @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
- * @version 0.9
+ * @author      zapalm <zapalm@ya.ru>
+ * @copyright   (c) 2011-2015, zapalm
+ * @link        http://prestashop.modulez.ru/en/ Homepage
+ * @license     http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ * @version     0.9
  */
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 class BlockBackOrder extends Module
 {
-	function __construct()
-    {
-        $this->name = 'blockbackorder';
-        $this->tab = 'Payment';
-        $this->version = '0.9';
-		$this->author = 'zapalm';
-		$this->need_instance = 0;
-		$this->ps_versions_compliancy = array('min' => '1.2.0.0', 'max' => '1.6.1.0');
-		$this->bootstrap = false;
-	
+    function __construct() {
+        $this->name          = 'blockbackorder';
+        $this->tab           = 'Payment';
+        $this->version       = '0.9';
+        $this->author        = 'zapalm';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.2.0.0', 'max' => '1.6.1.0');
+        $this->bootstrap     = false;
+
         parent::__construct();
 
         $this->displayName = $this->l('Block Back-order');
         $this->description = $this->l('Allow to back-ordering when a product out of stock');
     }
-   
-	public function install()
-	{
-		return parent::install() && $this->registerHook('productfooter');
-	}
 
-	public function uninstall()
-	{	
-		return parent::uninstall();
-	}
-	
-	function hookProductFooter($params)
-	{
-		global $smarty, $cookie;
+    public function install() {
+        return parent::install() && $this->registerHook('productfooter');
+    }
 
-		$error_code = null;
-		if (Tools::getIsset('bo_submit'))
-		{
-			if (Tools::getValue('firstname') && Tools::getValue('surname') && Tools::getValue('phone') && Tools::getValue('city') && Tools::getValue('comment') && Tools::getValue('email'))
-			{
-				$sent = Mail::Send(
-					intval($cookie->id_lang),
-					'backorder',
-					$this->l('Back Order'),
-					array(
-						'{firstname}' => Tools::getValue('firstname'),
-						'{surname}' => Tools::getValue('surname'),
-						'{email}' => Tools::getValue('email'),
-						'{city}' => Tools::getValue('city'),
-						'{phone}' => Tools::getValue('phone'),
-						'{comment}' => Tools::getValue('comment'),
-						'{product}' => $params['product']->name,
-					),
-					Configuration::get('PS_SHOP_EMAIL'),
-					null,
-					null,
-					null,
-					null,
-					null,
-					_PS_MODULE_DIR_.$this->name.'/mails/'
-				);
-				
-				$error_code = $sent ? 0 : 2;
-			}
-			else
-				$error_code = 1;
-		}
+    public function uninstall() {
+        return parent::uninstall();
+    }
 
-		if ($error_code == 1)
-			$msg = $this->l('All fields are requred.');
-		elseif ($error_code == 2)
-			$msg = $this->l('Unsuccesseful. Try again later.');
-		elseif ($error_code == 0)
-			$msg = $this->l('Successeful.');
-		else
-			$msg = '';
+    function hookProductFooter($params) {
+        global $smarty, $cookie;
 
-		$smarty->assign(array(
-			'msg' => $msg,
-			'error_code' => $error_code
-		));	
-	
-		return $this->display(__FILE__, 'blockbackorder.tpl');		
-	}	
+        $error_code = null;
+        if (Tools::getIsset('bo_submit')) {
+            if (Tools::getValue('firstname') && Tools::getValue('surname') && Tools::getValue('phone') && Tools::getValue('city') && Tools::getValue('comment') && Tools::getValue('email')) {
+                $sent = Mail::Send(
+                    intval($cookie->id_lang),
+                    'backorder',
+                    $this->l('Back Order'),
+                    array(
+                        '{firstname}' => Tools::getValue('firstname'),
+                        '{surname}' => Tools::getValue('surname'),
+                        '{email}' => Tools::getValue('email'),
+                        '{city}' => Tools::getValue('city'),
+                        '{phone}' => Tools::getValue('phone'),
+                        '{comment}' => Tools::getValue('comment'),
+                        '{product}' => $params['product']->name,
+                    ),
+                    Configuration::get('PS_SHOP_EMAIL'),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    _PS_MODULE_DIR_ . $this->name . '/mails/'
+                );
+
+                $error_code = $sent ? 0 : 2;
+            } else {
+                $error_code = 1;
+            }
+        }
+
+        if ($error_code == 1) {
+            $msg = $this->l('All fields are requred.');
+        } elseif ($error_code == 2) {
+            $msg = $this->l('Unsuccesseful. Try again later.');
+        } elseif ($error_code == 0) {
+            $msg = $this->l('Successeful.');
+        } else {
+            $msg = '';
+        }
+
+        $smarty->assign(array(
+            'msg'        => $msg,
+            'error_code' => $error_code,
+        ));
+
+        return $this->display(__FILE__, 'blockbackorder.tpl');
+    }
 }
--- blockbackorder.tpl
+++ blockbackorder.tpl
@@ -1,78 +1,80 @@
 <!-- MODULE blockbackorder -->
 <script type="text/javascript">
 // <![CDATA[
-	{if $allow_oosp && $product->quantity == 0}
-		{literal}
-		$("document").ready( function(){
-			$("#add_to_cart").hide();
-			$("#bo_div").show();
-		});
-		{/literal}
-	{/if}
+    {if $allow_oosp && $product->quantity == 0}
+        {literal}
+            $("document").ready(function () {
+                $("#add_to_cart").hide();
+                $("#bo_div").show();
+            });
+        {/literal}
+    {/if}
 //]]>
 </script>
 
 {literal}
-<STYLE TYPE="text/css">
-<!--
-  div.bo_title {
-    color: #374853;
-    font-size: 1.1em;
-    font-weight: bold;
-    height: 21px;
-    line-height: 1.6em;
-    margin: 0.5em 0;
-    padding-left: 0.5em;
-    text-transform: uppercase;  
-  }
-  
-  div.bo_description {
-	font-size: 1em;
-	font-weight: bold;
-    margin-left: 6px;
-    padding-bottom: 20px;	
-  }
-  
-  label.bo_label {
-    float: left;
-    padding: 0.2em 0 0;
-    text-align: left;
-    width: 76px;  
-	margin-left: 6px;
-  }
-  
-  div.bo_div {
-    padding-bottom: 10px;
-    padding-top: 10px;
-	display: none;
-  }
--->
-</STYLE>
+    <STYLE TYPE="text/css">
+        <!--
+        div.bo_title {
+            color: #374853;
+            font-size: 1.1em;
+            font-weight: bold;
+            height: 21px;
+            line-height: 1.6em;
+            margin: 0.5em 0;
+            padding-left: 0.5em;
+            text-transform: uppercase;
+        }
+
+        div.bo_description {
+            font-size: 1em;
+            font-weight: bold;
+            margin-left: 6px;
+            padding-bottom: 20px;
+        }
+
+        label.bo_label {
+            float: left;
+            padding: 0.2em 0 0;
+            text-align: left;
+            width: 76px;
+            margin-left: 6px;
+        }
+
+        div.bo_div {
+            padding-bottom: 10px;
+            padding-top: 10px;
+            display: none;
+        }
+        -->
+    </STYLE>
 {/literal}
 <div class="bo_div" id="bo_div">
-	<div class="bo_title">{l s='back-order' mod='blockbackorder'}</div>
-	{if isset($error_code)}<div class="{if $error_code > 0}error{else}success{/if}">{$msg}</div>{/if}
-	<div class="bo_description">{l s='Input information to back-order' mod='blockbackorder'}</div>
-	<form action="{$REQUEST_URI}" method="post">
-		<label class="bo_label">{l s='Name' mod='blockbackorder'}</label>
-		<input class="text" type="text" value="{$smarty.post.firstname}" name="firstname">
-		<br/><br/>			
-		<label class="bo_label">{l s='Surname' mod='blockbackorder'}</label>
-		<input class="text" type="text" value="{$smarty.post.surname}" name="surname">
-		<br/><br/>		
-		<label class="bo_label">{l s='Phone' mod='blockbackorder'}</label>
-		<input class="text" type="text" value="{$smarty.post.phone}" name="phone">
-		<br/><br/>
-		<label class="bo_label">{l s='E-mail' mod='blockbackorder'}</label>
-		<input class="text" type="text" value="{$smarty.post.email}" name="email">
-		<br/><br/>
-		<label class="bo_label">{l s='City' mod='blockbackorder'}</label>
-		<input class="text" type="text" value="{$smarty.post.city}" name="city">
-		<br/><br/>
-		<label class="bo_label">{l s='Comment' mod='blockbackorder'}</label>
-		<textarea cols="60" rows="3" name="comment">{$smarty.post.comment}</textarea>
-		<br/><br/>		
-		<center><input type="submit" name="bo_submit" value="{l s='Submit' mod='blockbackorder'}" class="button" /></center>
-	</form>
+    <div class="bo_title">{l s='back-order' mod='blockbackorder'}</div>
+    {if isset($error_code)}
+        <div class="{if $error_code > 0}error{else}success{/if}">{$msg}</div>{/if}
+    <div class="bo_description">{l s='Input information to back-order' mod='blockbackorder'}</div>
+    <form action="{$REQUEST_URI}" method="post">
+        <label class="bo_label">{l s='Name' mod='blockbackorder'}</label>
+        <input class="text" type="text" value="{$smarty.post.firstname}" name="firstname">
+        <br/><br/>
+        <label class="bo_label">{l s='Surname' mod='blockbackorder'}</label>
+        <input class="text" type="text" value="{$smarty.post.surname}" name="surname">
+        <br/><br/>
+        <label class="bo_label">{l s='Phone' mod='blockbackorder'}</label>
+        <input class="text" type="text" value="{$smarty.post.phone}" name="phone">
+        <br/><br/>
+        <label class="bo_label">{l s='E-mail' mod='blockbackorder'}</label>
+        <input class="text" type="text" value="{$smarty.post.email}" name="email">
+        <br/><br/>
+        <label class="bo_label">{l s='City' mod='blockbackorder'}</label>
+        <input class="text" type="text" value="{$smarty.post.city}" name="city">
+        <br/><br/>
+        <label class="bo_label">{l s='Comment' mod='blockbackorder'}</label>
+        <textarea cols="60" rows="3" name="comment">{$smarty.post.comment}</textarea>
+        <br/><br/>
+        <center><input type="submit" name="bo_submit" value="{l s='Submit' mod='blockbackorder'}" class="button"/>
+        </center>
+    </form>
 </div>
 <!-- /MODULE blockbackorder -->
